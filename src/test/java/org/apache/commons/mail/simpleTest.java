c3459ab347a10cebb7cbf51388843f83367caed7
DivorcedHorse <daniel.tellez99@outlook.com>
2020-11-14 14:55:02 -0600

    first test

---
+++ src/test/java/org/apache/commons/mail/simpleTest.java
@@ -0,0 +1,16 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class TemplateTest extends TestCase{
+	public void simple {
+		System.out.println("This is a message from the simple test.");
+		String result = "oneTwo";
+		assertEquals("oneTwo", result);
+	}
+
+}
