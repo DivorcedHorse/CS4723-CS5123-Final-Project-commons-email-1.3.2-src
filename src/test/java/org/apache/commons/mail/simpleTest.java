9a867a5952db32829ba68a7c7107e077bc57466a
DivorcedHorse <daniel.tellez99@outlook.com>
2020-11-14 15:02:16 -0600

    fixed issues

--- src/test/java/org/apache/commons/mail/simpleTest.java
+++ src/test/java/org/apache/commons/mail/simpleTest.java
@@ -6,7 +6,7 @@ import org.apache.commons.mail.SimpleEmail;
 
 import junit.framework.TestCase;
 
-public class simepleTest extends TestCase {
+public class simpleTest extends TestCase {
 
 	public void simple() {
 		System.out.println("This is a message from the simple test.");
